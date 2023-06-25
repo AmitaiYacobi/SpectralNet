5c898cdbaf0245c37b17bbefc1e7e9c94177808e
AmitaiYacobi <amitaiyacobi@gmail.com>
2023-06-25 19:14:28 +0300

    modified _spectral.py file which contains the main flow of the model

--- src/spectralnet/_utils.py
+++ src/spectralnet/_utils.py
@@ -8,52 +8,26 @@ from annoy import AnnoyIndex
 from sklearn.neighbors import NearestNeighbors
 
 
-
-def get_number_of_clusters(X: torch.Tensor,  n_samples: int, threshold: float) -> int:
-    """
-    Computes the number of clusters in the given dataset
-
-    Args:
-        X:          dataset
-        n_samples:  number of samples to use for computing the number of clusters
-        threshold:  threshold for the eigenvalues of the laplacian matrix. This 
-                    threshold is used in order to find when the difference between 
-                    the eigenvalues becomes large. 
-
-    Returns:
-        Number of clusters in the dataset
-    """
-    indices = torch.randperm(X.shape[0])[:n_samples]
-    X = X[indices]
-    
-    W = get_affinity_matrix(X)
-    L = get_laplacian(W)
-    vals = get_eigenvalues(L)
-    diffs = np.diff(vals)
-    cutoff = np.argmax(diffs > threshold)
-    num_clusters = cutoff + 1
-    return num_clusters
-
 def build_ann(X: torch.Tensor):
     """
-    Builds approximate-nearest-neighbors object 
+    Builds approximate-nearest-neighbors object
     that can be used to calculate the knn of a data-point
 
     Args:
         X:  dataset
     """
     X = X.view(X.size(0), -1)
-    t = AnnoyIndex(X[0].shape[0], 'euclidean')
+    t = AnnoyIndex(X[0].shape[0], "euclidean")
     for i, x_i in enumerate(X):
         t.add_item(i, x_i)
 
     t.build(50)
-    t.save('ann_index.ann')
+    t.save("ann_index.ann")
 
 
 def make_batch_for_sparse_grapsh(batch_x: torch.Tensor) -> torch.Tensor:
     """
-    Computes new batch of data points from the given batch (batch_x) 
+    Computes new batch of data points from the given batch (batch_x)
     in case that the graph-laplacian obtained from the given batch is sparse.
     The new batch is computed based on the nearest neighbors of 0.25
     of the given batch
@@ -71,8 +45,8 @@ def make_batch_for_sparse_grapsh(batch_x: torch.Tensor) -> torch.Tensor:
     batch_x = new_batch_x
     n_neighbors = 5
 
-    u = AnnoyIndex(batch_x[0].shape[0], 'euclidean')
-    u.load('ann_index.ann')
+    u = AnnoyIndex(batch_x[0].shape[0], "euclidean")
+    u.load("ann_index.ann")
     for x in batch_x:
         x = x.detach().cpu().numpy()
         nn_indices = u.get_nns_by_vector(x, n_neighbors)
@@ -89,7 +63,7 @@ def get_laplacian(W: torch.Tensor) -> np.ndarray:
 
     Args:
         W (torch.Tensor):   Affinity matrix
-    
+
     Returns:
         Laplacian matrix
     """
@@ -119,7 +93,7 @@ def sort_laplacian(L: np.ndarray, y: np.ndarray) -> np.ndarray:
     return L
 
 
-def sort_matrix_rows(A: np.ndarray , y: np.ndarray) -> np.ndarray:
+def sort_matrix_rows(A: np.ndarray, y: np.ndarray) -> np.ndarray:
     """
     Sorts the rows of a matrix by a given order y
 
@@ -163,6 +137,7 @@ def get_eigenvectors(A: np.ndarray) -> np.ndarray:
     vecs = vecs[:, np.argsort(vals)]
     return vecs
 
+
 def plot_eigenvalues(vals: np.ndarray):
     """
     Plot the eigenvalues of the laplacian
@@ -174,6 +149,7 @@ def plot_eigenvalues(vals: np.ndarray):
     rang = range(len(vals))
     plt.plot(rang, vals)
     plt.show()
+
 
 def get_laplacian_eigenvectors(V: torch.Tensor, y: np.ndarray) -> np.ndarray:
     """
@@ -188,6 +164,7 @@ def get_laplacian_eigenvectors(V: torch.Tensor, y: np.ndarray) -> np.ndarray:
     V = sort_matrix_rows(V, y)
     rang = range(len(y))
     return V, rang
+
 
 def plot_laplacian_eigenvectors(V: np.ndarray, y: np.ndarray):
     """
@@ -217,14 +194,16 @@ def plot_sorted_laplacian(W: torch.Tensor, y: np.ndarray):
     """
     L = get_laplacian(W)
     L = sort_laplacian(L, y)
-    plt.imshow(L, cmap='hot', norm=colors.LogNorm())
-    plt.imshow(L, cmap='flag')
+    plt.imshow(L, cmap="hot", norm=colors.LogNorm())
+    plt.imshow(L, cmap="flag")
     plt.show()
 
 
-def get_nearest_neighbors(X: torch.Tensor, Y: torch.Tensor = None, k: int = 3) -> tuple[np.ndarray, np.ndarray]:
+def get_nearest_neighbors(
+    X: torch.Tensor, Y: torch.Tensor = None, k: int = 3
+) -> tuple[np.ndarray, np.ndarray]:
     """
-    Computes the distances and the indices of the 
+    Computes the distances and the indices of the
     k nearest neighbors of each data point
 
     Args:
@@ -263,7 +242,9 @@ def get_grassman_distance(A: np.ndarray, B: np.ndarray) -> float:
     return grassmann
 
 
-def compute_scale(Dis: np.ndarray, k: int = 2, med: bool = True, is_local: bool = True) -> np.ndarray:
+def compute_scale(
+    Dis: np.ndarray, k: int = 2, med: bool = True, is_local: bool = True
+) -> np.ndarray:
     """
     Computes the scale for the Gaussian similarity function
 
@@ -271,7 +252,7 @@ def compute_scale(Dis: np.ndarray, k: int = 2, med: bool = True, is_local: bool 
         Dis:            Distances of the k nearest neighbors of each data point.
         k (optional):   Number of nearest neighbors for the scale calculation. Relevant for global scale only.
         med:            Scale calculation method. Can be calculated by the median distance
-                        from a data point to its neighbors, or by the maximum distance. 
+                        from a data point to its neighbors, or by the maximum distance.
         is_local:       Local distance (different for each data point), or global distance. Defaults to local.
 
     Returns:
@@ -291,17 +272,19 @@ def compute_scale(Dis: np.ndarray, k: int = 2, med: bool = True, is_local: bool 
     return scale
 
 
-def get_gaussian_kernel(D: torch.Tensor, scale, Ids: np.ndarray, device: torch.device, is_local: bool = True) -> torch.Tensor:   
+def get_gaussian_kernel(
+    D: torch.Tensor, scale, Ids: np.ndarray, device: torch.device, is_local: bool = True
+) -> torch.Tensor:
     """
-    Computes the Gaussian similarity function 
+    Computes the Gaussian similarity function
     according to a given distance matrix D and a given scale
 
     Args:
-        D:      Distance matrix 
+        D:      Distance matrix
         scale:  scale
         Ids:    Indices of the k nearest neighbors of each sample
         device: Defaults to torch.device("cpu")
-        is_local:  Determines whether the given scale is global or local 
+        is_local:  Determines whether the given scale is global or local
 
     Returns:
         Matrix W with Gaussian similarities
@@ -309,17 +292,20 @@ def get_gaussian_kernel(D: torch.Tensor, scale, Ids: np.ndarray, device: torch.d
 
     if not is_local:
         # global scale
-        W = torch.exp(-torch.pow(D, 2) / (scale ** 2))
+        W = torch.exp(-torch.pow(D, 2) / (scale**2))
     else:
         # local scales
-        W = torch.exp(-torch.pow(D, 2).to(device) / (torch.tensor(scale).float().to(device).clamp_min(1e-7) ** 2))
+        W = torch.exp(
+            -torch.pow(D, 2).to(device)
+            / (torch.tensor(scale).float().to(device).clamp_min(1e-7) ** 2)
+        )
     if Ids is not None:
         n, k = Ids.shape
         mask = torch.zeros([n, n]).to(device=device)
         for i in range(len(Ids)):
             mask[i, Ids[i]] = 1
         W = W * mask
-    sym_W = (W + torch.t(W)) / 2.
+    sym_W = (W + torch.t(W)) / 2.0
     return sym_W
 
 
@@ -330,13 +316,14 @@ def plot_data_by_assignmets(X, assignments: np.ndarray):
 
     Args:
         X:                      Data
-        cluster_assignments:    Cluster assignments 
+        cluster_assignments:    Cluster assignments
     """
 
     plt.scatter(X[:, 0], X[:, 1], c=assignments)
     plt.show()
 
-def calculate_cost_matrix(C: np.ndarray , n_clusters: int) -> np.ndarray:
+
+def calculate_cost_matrix(C: np.ndarray, n_clusters: int) -> np.ndarray:
     """
     Calculates the cost matrix for the Munkres algorithm
 
@@ -383,16 +370,14 @@ def write_assignmets_to_file(assignments: np.ndarray):
         assignments (np.ndarray): The assignments that obtained from SpectralNet
     """
 
-    np.savetxt("cluster_assignments.csv", assignments.astype(int), fmt='%i', delimiter=',')
+    np.savetxt(
+        "cluster_assignments.csv", assignments.astype(int), fmt="%i", delimiter=","
+    )
 
 
 def create_weights_dir():
     """
     Creates a directory for the weights of the Autoencoder and the Siamese network
     """
-    if not os.path.exists('weights'):
-        os.makedirs('weights')
-
-
-
-
+    if not os.path.exists("weights"):
+        os.makedirs("weights")
